dc1749269fd664e5802208e16106537107b56263
iraravi <ravidorairaj@icloud.com>
2025-01-01 15:18:51 -0600

    first commit

--- src/main/java/com/kira/ira/expensetrackerapi/config/WebConfig.java
+++ src/main/java/com/kira/ira/expensetrackerapi/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**") // Allow all endpoints
-                        .allowedOrigins("http://localhost:61088") // Allow Angular app origin
+                        .allowedOrigins("*") // Allow Angular app origin
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allow all HTTP methods
                         .allowedHeaders("*") // Allow all headers
                         .allowCredentials(true); // Allow credentials (if needed)
